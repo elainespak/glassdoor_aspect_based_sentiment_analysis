64edb0ac22528074c479b536d2f0f57b92783e36
elainespak <elainespak@gmail.com>
2020-08-10 16:04:22 +0900

    [Python] Make changes to the baseline bert code

--- baseline_bert/bert.py
+++ baseline_bert/bert.py
@@ -1,11 +1,52 @@
 # -*- coding: utf-8 -*-
 
-
+import re
 import torch
 import logging
 from tqdm import tqdm
+from nltk import sent_tokenize
 from scipy.spatial.distance import cosine
 from transformers import BertTokenizer, BertModel
+
+
+def load_only_text(master, text_type, company=False):
+
+    if company==False:
+        pass
+    else:
+        # if company is specified, filter for only that company's review data
+        master = master[master['company']==company]
+        print(f'{company} text loaded!')
+    
+    # Combine all texts regardless of text_type
+    all_sentences = []
+    for t in text_type:
+        all_sentences += list(master[t])
+    
+    return all_sentences
+
+
+def preprocess_sentence_tokenize(raw_sentences):
+    """
+    ###  INPUT
+    # raw_sentences: list of raw sentences
+    ###  OUTPUT
+    # tokenized_sentences: list of processed sentences
+    """
+    tokenized_sentences = []
+    
+    for raw in raw_sentences:
+        
+        # Change for proper sentence tokenization
+        raw = re.sub('\r\n|\n-|\n|\r','. ', raw)
+        raw = re.sub(',\.+ ', ', ', raw)
+        raw = re.sub('\.+ ', '. ', raw)
+        raw = re.sub('&amp;', '&', raw)
+        
+        # Sentence tokenization
+        sentences = sent_tokenize(raw)
+        tokenized_sentences += sentences
+    return tokenized_sentences
 
 
 class BERTEmbedding:
@@ -20,17 +61,22 @@ class BERTEmbedding:
 
     def retrieve_bert(self):
         tokenizer = BertTokenizer.from_pretrained(self.model_type,
-                                                  output_hidden_states = True)
+                                                  output_hidden_states = True
+                                                  )
         model = BertModel.from_pretrained(self.model_type,
-                                  output_hidden_states = True, # Whether the model returns all hidden-states.
-                                  )
+                                          # Whether the model returns all hidden-states.
+                                          output_hidden_states = True
+                                          )
         return model.to(self.device), tokenizer
+        #return model, tokenizer
 
     def get_embeddings(self, sent):
         marked_sent = '[CLS] ' + sent + ' [SEP]'
         
         # Split the sentence into tokens.
         tokenized_sent = self.tokenizer.tokenize(marked_sent)
+        if len(tokenized_sent) > 512:
+            tokenized_sent = tokenized_sent[:512]
 
         # Map the token strings to their vocabulary indeces.
         indexed_tokens = self.tokenizer.convert_tokens_to_ids(tokenized_sent)
@@ -69,53 +115,41 @@ class BERTEmbedding:
 
 if __name__ == "__main__":
      
-     # Check for cpu / gpu
-     is_cuda = torch.cuda.is_available()
-     if is_cuda:
-          device = torch.device("cuda")
-     else:
-          device = torch.device("cpu")
-     print(device)
-     
-     # Set up
-     bert_embedding = BERTEmbedding(device, 'bert-base-uncased')
-     
-     # Call data
-     master = torch.load('C:/Users/elain/Desktop/glassdoor_aspect_based_sentiment_analysis/sample_data/2008 to 2018 SnP 500 Firm Data_Master English Files/english_glassdoor_reviews_text_preprocessed.pt')
-     all_tokenized_sentences = []
-     for col in master:
-         if col=='pros' or col=='cons' or col=='advice':
-             all_tokenized_sentences += list(master[col])
-             
-     bert_sentences = []
-     for sent in tqdm(all_tokenized_sentences):
-         bert_sentences.append(bert_embedding.get_embeddings(sent))
-     
-     """
-     with open(path, 'r') as f:
-         f = f.readlines()
-         for sent in tqdm(f):
-             sentence_bert.append(bert_embedding.get_embeddings(sent))
-     torch.save('C:/Users/elain/Desktop/glassdoor_aspect_based_sentiment_analysis/sample_data/abae/glassdoor/test.pt')
-     print('Done!')
-     
-     path = 'C:/Users/elain/Desktop/glassdoor_aspect_based_sentiment_analysis/sample_data/abae/glassdoor/train.txt'
-     sentence_bert = []
-     with open(path, 'r') as f:
-         f = f.readlines()
-         for sent in tqdm(f):
-             sentence_bert.append(bert_embedding.get_embeddings(sent))
-     torch.save('C:/Users/elain/Desktop/glassdoor_aspect_based_sentiment_analysis/sample_data/abae/glassdoor/train.pt')
-     print('Done!')
-     
+    # Check for cpu / gpu
+    is_cuda = torch.cuda.is_available()
+    if is_cuda:
+         device = torch.device("cuda")
+    else:
+         device = torch.device("cpu")
+    print(device)
+    
+    # Set up
+    bert_embedding = BERTEmbedding(device, 'bert-base-uncased')
+    
+    # Call data
+    master = torch.load('C:/Users/elain/Desktop/glassdoor_aspect_based_sentiment_analysis/sample_data/2008 to 2018 SnP 500 Firm Data_Master English Files/english_glassdoor_reviews_text_preprocessed.pt')
+    all_raw_sentences = load_only_text(master, ['pros', 'cons', 'advice'])
+    del master
+    print('Done loading data!\n')
+    
+    all_tokenized_sentences = preprocess_sentence_tokenize(all_raw_sentences)
+    print('Done tokenizing!\n')
+    bert_sentences = []
+    for sent in tqdm(all_tokenized_sentences):
+        bert_sentences.append(bert_embedding.get_embeddings(sent).cpu())
+    # 3164840만큼 진행함
+    torch.save(bert_sentences,
+               'C:/Users/elain/Desktop/glassdoor_aspect_based_sentiment_analysis/sample_data/baseline_bert/english_bert_sentence_embeddings.pt')
      
         
+        
+    """
      
-     test1=bert_embedding.get_embeddings('the best company ever')
-     test2=bert_embedding.get_embeddings('toxic work environment')
-     test3=bert_embedding.get_embeddings('great place to work for')
-     
-     
-     diff_1 = 1-cosine(test1.cpu(), test2.cpu())
-     diff_2 = 1-cosine(test1.cpu(), test3.cpu())
-     """
+    test1=bert_embedding.get_embeddings('the best company ever')
+    test2=bert_embedding.get_embeddings('toxic work environment')
+    test3=bert_embedding.get_embeddings('great place to work for')
+    
+    
+    diff_1 = 1-cosine(test1.cpu(), test2.cpu())
+    diff_2 = 1-cosine(test1.cpu(), test3.cpu())
+    """
