a7cb55dc98c4bb69bdd5ffec7be08600bb3ba42b
elainespak <elainespak@gmail.com>
2020-08-15 23:37:49 +0900

    [Python] Update text preprocessing code

--- abae/text_preprocessing.py
+++ abae/text_preprocessing.py
@@ -8,16 +8,16 @@ from tqdm import tqdm
 pd.set_option('display.max_columns', 500)
 
 
-def make_train_test(all_tokenized_sentences, domain, test_size):
+def make_train_test(all_tokenized_sentences, outputpath, domain, test_size):
     train = all_tokenized_sentences[:-test_size]
     test = all_tokenized_sentences[-test_size:]
     
-    out = codecs.open('../sample_data/abae/'+domain+'/train.txt', 'w', 'utf-8')
+    out = codecs.open(outputpath+domain+'/train.txt', 'w', 'utf-8')
     for review in tqdm(train):
         for tokens in review:
             if len(tokens) > 1:
                 out.write(' '.join(tokens)+'\n')
-    out = codecs.open('../sample_data/abae/'+domain+'/test.txt', 'w', 'utf-8')
+    out = codecs.open(outputpath+domain+'/test.txt', 'w', 'utf-8')
     for review in tqdm(test):
         for tokens in review:
             if len(tokens) > 1:
@@ -29,6 +29,7 @@ if __name__ == "__main__":
     
     parser = argparse.ArgumentParser()
     parser.add_argument('--masterpath', type=str, default='../sample_data/2008 to 2018 SnP 500 Firm Data_Master English Files/english_glassdoor_reviews_text_preprocessed.pt')
+    parser.add_argument('--outputpath', type=str, default='../preprocessed_data/')
     parser.add_argument('--domain', type=str)
     parser.add_argument('--tokentype', type=str)
     parser.add_argument('--testsize', type=int, default=2000)
@@ -42,4 +43,4 @@ if __name__ == "__main__":
             all_tokenized_sentences += list(master[col])
     print('Done loading data!')
     
-    make_train_test(all_tokenized_sentences, args.domain, args.testsize)
+    make_train_test(all_tokenized_sentences, args.outputpath, args.domain, args.testsize)
