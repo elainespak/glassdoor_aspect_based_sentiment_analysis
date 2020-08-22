3d6a866491b104360aa95dca42639a6d2c9cc592
elainespak <elainespak@gmail.com>
2020-08-22 19:11:25 +0900

    [Python] #3 Update aspect matching code with original sentences for BERT

--- sentence_classification/aspect_matching.py
+++ sentence_classification/aspect_matching.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
 import os
+import torch
 import pandas as pd
 from tqdm import tqdm
 pd.set_option('display.max_columns', 500)
@@ -17,79 +18,73 @@ def label_aspect(a1, a2, a3, dictionary):
     else:
         return a1
 
-def create_input(att_path, aspect, aspect_dictionary):
+
+def create_input(att_path, aspect_dictionary):
     
     with open(att_path, 'r') as f:
         att = f.read().split('----------------------------------------')
-    with open(f'../abae/preprocessed_data/glassdoor/gold/{aspect}/{aspect}_000.txt', 'r') as f:
-        gold = f.read().strip().split('\n')
     
     sent_dict = []
     for result in att[1:]:
         sent = result.strip().split('\n')
         temp = {'aspect_'+str(i): sent[i] for i in range(1,4)}
-        temp['sentence'] = sent[4]
+        temp['tokenized_sentence'] = sent[4]
         temp['att'] = sent[5:]
         sent_dict.append(temp)
     
     df = pd.DataFrame(sent_dict)
     df['aspect'] = df['aspect_1'].apply(lambda a: d[a.split(':')[0]])
     #df['aspect'] = df.apply(lambda x: label_aspect(x['aspect_1'], x['aspect_2'], x['aspect_3'], d), axis=1)
-    df[aspect] = gold
-    aspect_df = df[df.aspect == aspect]
-    return aspect_df
-    
+    return df
+
+
+def filter_by_aspect(data, aspect):
+    data_aspect = data[data['aspect'] == aspect]
+    data_aspect = data_aspect[['original', 'rating'+aspect]]
+    data_aspect = data_aspect.reset_index(drop=True)
+    return data_aspect
+
 
 if __name__ == '__main__':
-    
+
     # Parameters
     path = '../abae/output_dir/glassdoor/aspect_size_20/tests/'
-    aspect = 'ratingCompensationAndBenefits'
-    d = {'None': 'None',
-         'Technical': '?', 'Restructuring': '?', 'Structure and Policies': '?', 'Customers, Products, and Services': '?', 'Workspace': '?', 'Location': '?',
+    d = {# None
+         'None': 'None',
+         # ?
+         'Technical': '?', 'Restructuring': '?', 'Structure and Policies': '?',
+         'Customers, Products, and Services': '?', 'Workspace': '?', 'Location': '?',
+         # Overall
          'Overall': 'Overall',
+         # CompensationAndBenefits
          'Benefits': 'CompensationAndBenefits', 'Perks': 'CompensationAndBenefits', 'Compensation': 'CompensationAndBenefits',
+         # CultureAndValues
          'Moral Values': 'CultureAndValues', 'Culture': 'CultureAndValues', 'People': 'CultureAndValues',
+         # WorkLifeBalance
          'Working Conditions': 'WorkLifeBalance', 'Work Life Balance': 'WorkLifeBalance',
+         #SeniorLeadership
          'Senior Leadership': 'SeniorLeadership',
-         'Career Opportunities: Junior Perspective': 'CareerOpportunities', 'Career Opportunities: Senior Perspective': 'CareerOpportunities', 'Career Opportunities': 'CareerOpportunities'}
+         # CareerOpportunities
+         'Career Opportunities: Junior Perspective': 'CareerOpportunities',
+         'Career Opportunities: Senior Perspective': 'CareerOpportunities',
+         'Career Opportunities': 'CareerOpportunities'}
+    aspects = {'Overall', 'CompensationAndBenefits', 'CultureAndValues', 'WorkLifeBalance', 'SeniorLeadership', 'CareerOpportunities'}
     
     # Make input data for sentence classification model
-    data = pd.DataFrame()
+    label = pd.DataFrame()
     for att_file in tqdm(os.listdir(path)):
-        path += att_file
-        data = pd.concat([data, create_input(path, aspect, d)])
-    data.to_csv(f'../sample_data/sentence_classification/{aspect}.csv')
+        filepath = path + att_file
+        temp = create_input(filepath, d)[['tokenized_sentence', 'aspect']]
+        label = pd.concat([label, temp])
+    label = label.reset_index(drop=True)
     
+    origin = torch.load('../abae/preprocessed_data/glassdoor/gold/original_english_review_exploded.pt')
+    origin = origin[['original']+[col for col in origin.columns if col.startswith('rating')]]
     
+    data = pd.concat([origin, label], axis=1)
     
-path='../abae/output_dir/glassdoor/aspect_size_20/tests/att_weights_all_tokenized_trigram_sentences_000.txt.txt'
-with open(path, 'r') as f:
-    test = f.read().split('----------------------------------------')
-
-sent_dict = []
-for result in test[1:]:
-    sent = result.strip().split('\n')
-    temp = {'aspect_'+str(i): sent[i] for i in range(1,4)}
-    temp['sentence'] = sent[4]
-    temp['att'] = sent[5:]
-    sent_dict.append(temp)
-
-df = pd.DataFrame(sent_dict)
-len(df[df.ratingCompensationAndBenefits == '0.0']) / len(df)
-
-
-    
-df['aspect'] = df['aspect_1'].apply(lambda a: d[a.split(':')[0]])
-#df['aspect'] = df.apply(lambda x: label_aspect(x['aspect_1'], x['aspect_2'], x['aspect_3'], d), axis=1)
-
-with open('../abae/preprocessed_data/glassdoor/gold/ratingCompensationAndBenefits/ratingCompensationAndBenefits_000.txt', 'r') as f:
-    gold = f.read().strip().split('\n')
-    
-df['ratingCompensationAndBenefits'] = gold
-
-test = df[df.aspect == 'CompensationAndBenefits']
-
-len(test[test.ratingCompensationAndBenefits == '0.0']) / len(test)
-
-
+    for aspect in aspects:
+        data_aspect = filter_by_aspect(data, aspect)
+        torch.save(data_aspect, f'../sample_data/sentence_classification/{aspect}_for_sentence_classification.pt')
+        print(f'Done with {aspect}')
+       
