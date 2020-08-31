d97c367ec25b7e46705f85867cdae517b36b5ae8
elainespak <elainespak@gmail.com>
2020-08-31 17:05:08 +0900

    [Python] #2 Captum for BERT sentence classification task

--- sentence_classification/captum.py
+++ sentence_classification/captum.py
@@ -30,7 +30,7 @@ if device.type == 'cuda':
 aspect = 'CompensationAndBenefits'
 model_path = 'bert-base-uncased'
 PRE_TRAINED_MODEL_NAME = f'C:/Users/elain/Desktop/glassdoor_aspect_based_sentiment_analysis/sentence_classification/toy_{aspect}/'
-class_names = ['negative', 'positive']
+#class_names = ['negative', 'positive']
 
 model = BertForSequenceClassification.from_pretrained(PRE_TRAINED_MODEL_NAME)
 model.to(device)
@@ -40,14 +40,12 @@ model.zero_grad()
 # load tokenizer
 tokenizer = BertTokenizer.from_pretrained(model_path)
 
-def predict(inputs, token_type_ids=None, position_ids=None, attention_mask=None):
-    return model(inputs, token_type_ids=token_type_ids,
-                 position_ids=position_ids, attention_mask=attention_mask, )
+def predict(inputs, token_type_ids=None, attention_mask=None):
+    return model(inputs, token_type_ids=token_type_ids, attention_mask=attention_mask, )
     
-def squad_pos_forward_func(inputs, token_type_ids=None, position_ids=None, attention_mask=None, position=0):
+def sentclass_pos_forward_func(inputs, token_type_ids=None, attention_mask=None, position=0):
     pred = predict(inputs,
                    token_type_ids=token_type_ids,
-                   position_ids=position_ids,
                    attention_mask=attention_mask)
     pred = pred[position]
     return pred.max(1).values
@@ -73,78 +71,54 @@ def construct_input_ref_token_type_pair(input_ids, sep_ind=0):
     ref_token_type_ids = torch.zeros_like(token_type_ids, device=device)# * -1
     return token_type_ids, ref_token_type_ids
 
-def construct_input_ref_pos_id_pair(input_ids):
-    seq_length = input_ids.size(1)
-    position_ids = torch.arange(seq_length, dtype=torch.long, device=device)
-    # we could potentially also use random permutation with `torch.randperm(seq_length, device=device)`
-    ref_position_ids = torch.zeros(seq_length, dtype=torch.long, device=device)
-
-    position_ids = position_ids.unsqueeze(0).expand_as(input_ids)
-    ref_position_ids = ref_position_ids.unsqueeze(0).expand_as(input_ids)
-    return position_ids, ref_position_ids
-    
 def construct_attention_mask(input_ids):
     return torch.ones_like(input_ids)
 
 def construct_bert_sub_embedding(input_ids, ref_input_ids,
                                    token_type_ids, ref_token_type_ids,
-                                   position_ids, ref_position_ids):
+                                   ref_position_ids):
     input_embeddings = interpretable_embedding1.indices_to_embeddings(input_ids)
     ref_input_embeddings = interpretable_embedding1.indices_to_embeddings(ref_input_ids)
 
     input_embeddings_token_type = interpretable_embedding2.indices_to_embeddings(token_type_ids)
     ref_input_embeddings_token_type = interpretable_embedding2.indices_to_embeddings(ref_token_type_ids)
 
-    input_embeddings_position_ids = interpretable_embedding3.indices_to_embeddings(position_ids)
-    ref_input_embeddings_position_ids = interpretable_embedding3.indices_to_embeddings(ref_position_ids)
-    
     return (input_embeddings, ref_input_embeddings), \
-           (input_embeddings_token_type, ref_input_embeddings_token_type), \
-           (input_embeddings_position_ids, ref_input_embeddings_position_ids)
+           (input_embeddings_token_type, ref_input_embeddings_token_type)
     
 def construct_whole_bert_embeddings(input_ids, ref_input_ids, \
                                     token_type_ids=None, ref_token_type_ids=None, \
-                                    position_ids=None, ref_position_ids=None):
-    input_embeddings = interpretable_embedding.indices_to_embeddings(input_ids, token_type_ids=token_type_ids, position_ids=position_ids)
-    ref_input_embeddings = interpretable_embedding.indices_to_embeddings(ref_input_ids, token_type_ids=token_type_ids, position_ids=position_ids)
+                                    ref_position_ids=None):
+    input_embeddings = interpretable_embedding.indices_to_embeddings(input_ids, token_type_ids=token_type_ids)
+    ref_input_embeddings = interpretable_embedding.indices_to_embeddings(ref_input_ids, token_type_ids=token_type_ids)
     
     return input_embeddings, ref_input_embeddings
 
 
-text = "Love the people, very healthy communications"
+text = "Wonderful benefits and pay"
 
 input_ids, ref_input_ids, sep_id = construct_input_ref_pair(text, ref_token_id, sep_token_id, cls_token_id)
 token_type_ids, ref_token_type_ids = construct_input_ref_token_type_pair(input_ids, sep_id)
-position_ids, ref_position_ids = construct_input_ref_pos_id_pair(input_ids)
 attention_mask = construct_attention_mask(input_ids)
 
-indices = input_ids[0].detach().tolist()
+indices = input_ids[0].detach().tolist() # take off from cuda to cpu
 all_tokens = tokenizer.convert_ids_to_tokens(indices)
 
 
-ground_truth = 'to include, empower and support humans of all kinds'
-ground_truth_tokens = tokenizer.encode(ground_truth, add_special_tokens=False)
-ground_truth_end_ind = indices.index(ground_truth_tokens[-1])
-ground_truth_start_ind = ground_truth_end_ind - len(ground_truth_tokens) + 1
+ground_truth = 1
 
-start_scores, end_scores = predict(input_ids, \
-                                   token_type_ids=token_type_ids, \
-                                   position_ids=position_ids, \
-                                   attention_mask=attention_mask)
+logits = predict(input_ids, token_type_ids=token_type_ids, attention_mask=attention_mask)
 
 
-print('Question: ', text)
-print('Predicted Answer: ', ' '.join(all_tokens[torch.argmax(start_scores) : torch.argmax(end_scores)+1]))
+print('Review sentence: ', text)
+print(f'Predicted Answer: {torch.argmax(logits[0])}')
 
-lig = LayerIntegratedGradients(squad_pos_forward_func, model.bert.embeddings)
+lig = LayerIntegratedGradients(sentclass_pos_forward_func, model.bert.embeddings)
 
 attributions_start, delta_start = lig.attribute(inputs=input_ids,
                                   baselines=ref_input_ids,
-                                  additional_forward_args=(token_type_ids, position_ids, attention_mask, 0),
+                                  additional_forward_args=(token_type_ids, attention_mask),
                                   return_convergence_delta=True)
-attributions_end, delta_end = lig.attribute(inputs=input_ids, baselines=ref_input_ids,
-                                additional_forward_args=(token_type_ids, position_ids, attention_mask, 1),
-                                return_convergence_delta=True)
 
 def summarize_attributions(attributions):
     attributions = attributions.sum(dim=-1).squeeze(0)
@@ -152,37 +126,23 @@ def summarize_attributions(attributions):
     return attributions
 
 attributions_start_sum = summarize_attributions(attributions_start)
-attributions_end_sum = summarize_attributions(attributions_end)
 
 # storing couple samples in an array for visualization purposes
 start_position_vis = viz.VisualizationDataRecord(
                         attributions_start_sum,
-                        torch.max(torch.softmax(start_scores[0], dim=0)),
-                        torch.argmax(start_scores),
-                        torch.argmax(start_scores),
-                        str(ground_truth_start_ind),
+                        torch.max(torch.softmax(logits[0], dim=1)),
+                        torch.argmax(logits[0]),
+                        torch.argmax(logits[0]),
+                        str(ground_truth),
                         attributions_start_sum.sum(),       
                         all_tokens,
                         delta_start)
 
-end_position_vis = viz.VisualizationDataRecord(
-                        attributions_end_sum,
-                        torch.max(torch.softmax(end_scores[0], dim=0)),
-                        torch.argmax(end_scores),
-                        torch.argmax(end_scores),
-                        str(ground_truth_end_ind),
-                        attributions_end_sum.sum(),       
-                        all_tokens,
-                        delta_end)
 
 print('\033[1m', 'Visualizations For Start Position', '\033[0m')
 viz.visualize_text([start_position_vis])
 
-print('\033[1m', 'Visualizations For End Position', '\033[0m')
-viz.visualize_text([end_position_vis])
 
-from IPython.display import Image
-Image(filename='img/bert/visuals_of_start_end_predictions.png')
 
 
 
