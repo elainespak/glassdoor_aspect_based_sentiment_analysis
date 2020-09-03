c5193293dea2b4b3e098c6294ae3a7d31b10e76f
elainespak <elainespak@gmail.com>
2020-09-03 18:47:57 +0900

    [Python] Refactorize the attribution model code

--- sentence_classification/attribution_model.py
+++ sentence_classification/attribution_model.py
@@ -14,69 +14,6 @@ from captum.attr import LayerIntegratedGradients
 from transformers import BertTokenizer, BertForSequenceClassification
 
 
-def predict(inputs, token_type_ids=None, attention_mask=None):
-    return model(inputs, token_type_ids=token_type_ids, attention_mask=attention_mask, )
-    
-def sentclass_pos_forward_func(inputs, token_type_ids=None, attention_mask=None, position=0):
-    pred = predict(inputs,
-                   token_type_ids=token_type_ids,
-                   attention_mask=attention_mask)
-    pred = pred[position]
-    return pred.max(1).values
-
-def construct_input_ref_pair(text, ref_token_id, sep_token_id, cls_token_id):
-    text_ids = tokenizer.encode(text, add_special_tokens=False)
-
-    # construct input token ids
-    input_ids = [cls_token_id] + text_ids + [sep_token_id]
-
-    # construct reference token ids 
-    ref_input_ids = [cls_token_id] + [ref_token_id] * len(text_ids) + [sep_token_id]
-
-    return torch.tensor([input_ids], device=device), torch.tensor([ref_input_ids], device=device), len(text_ids)
-
-def construct_input_ref_token_type_pair(input_ids, sep_ind=0):
-    seq_len = input_ids.size(1)
-    token_type_ids = torch.tensor([[0 if i <= sep_ind else 1 for i in range(seq_len)]], device=device)
-    ref_token_type_ids = torch.zeros_like(token_type_ids, device=device)# * -1
-    return token_type_ids, ref_token_type_ids
-
-def construct_attention_mask(input_ids):
-    return torch.ones_like(input_ids)
-
-def construct_bert_sub_embedding(input_ids, ref_input_ids,
-                                   token_type_ids, ref_token_type_ids,
-                                   ref_position_ids):
-    input_embeddings = interpretable_embedding1.indices_to_embeddings(input_ids)
-    ref_input_embeddings = interpretable_embedding1.indices_to_embeddings(ref_input_ids)
-
-    input_embeddings_token_type = interpretable_embedding2.indices_to_embeddings(token_type_ids)
-    ref_input_embeddings_token_type = interpretable_embedding2.indices_to_embeddings(ref_token_type_ids)
-
-    return (input_embeddings, ref_input_embeddings), \
-           (input_embeddings_token_type, ref_input_embeddings_token_type)
-    
-def construct_whole_bert_embeddings(input_ids, ref_input_ids, \
-                                    token_type_ids=None, ref_token_type_ids=None, \
-                                    ref_position_ids=None):
-    input_embeddings = interpretable_embedding.indices_to_embeddings(input_ids, token_type_ids=token_type_ids)
-    ref_input_embeddings = interpretable_embedding.indices_to_embeddings(ref_input_ids, token_type_ids=token_type_ids)
-    
-    return input_embeddings, ref_input_embeddings
-
-def summarize_attributions(attributions):
-    attributions = attributions.sum(dim=-1).squeeze(0)
-    attributions = attributions / torch.norm(attributions)
-    return attributions
-
-def to_sentiment(rating):
-    rating = float(rating)
-    if rating <= 2:
-        return 0
-    else:
-        return 1
-
-
 class FineTunedBertForAttribution:
     task = 'BertForSequenceClassification'
     
@@ -172,7 +109,15 @@ class FineTunedBertForAttribution:
         print(f'Predicted Answer: {self.prediction} vs. Gold: {self.ground_truth}')
         for t, a in zip(self.all_tokens, self.attributions_start_summary):
             print(t + '\t\t' + str(a))
-    
+
+
+def to_sentiment(rating):
+    rating = float(rating)
+    if rating <= 2:
+        return 0
+    else:
+        return 1
+ 
 
 if __name__ == "__main__":
     
@@ -189,11 +134,11 @@ if __name__ == "__main__":
     aspect = 'CompensationAndBenefits'
     epochs = 3
     kind = 'all'
-    #FINE_TUNED_MODEL_NAME = f'C:/Users/elain/Desktop/glassdoor_aspect_based_sentiment_analysis/sentence_classification/{aspect}_{kind}_epoch{str(epochs)}/'
-    FINE_TUNED_MODEL_NAME = f'C:/Users/elain/Desktop/glassdoor_aspect_based_sentiment_analysis/sentence_classification/toy_{aspect}/'
+    #FINE_TUNED_MODEL_NAME = f'./{aspect}_{kind}_epoch{str(epochs)}/'
+    FINE_TUNED_MODEL_NAME = f'./toy_{aspect}/'
     
     aspect = 'CompensationAndBenefits'
-    df = torch.load(f'C:/Users/elain/Desktop/glassdoor_aspect_based_sentiment_analysis/sample_data/sentence_classification/{aspect}_for_sentence_classification.pt')
+    df = torch.load(f'../sample_data/sentence_classification/{aspect}_for_sentence_classification.pt')
     df['labels'] = df['rating'+aspect].apply(to_sentiment)
     df = df[:5000]
     
@@ -207,46 +152,9 @@ if __name__ == "__main__":
     attribution_model.construct_input_ref_token_type_pair()
     attribution_model.construct_attention_mask()
     attribution_model.predict_label()
-    #attribution_model.sentclass_pos_forward_func()
     attribution_model.attribution()
     attribution_model.show_results(ground_truth)
-    
-    
-    model = BertForSequenceClassification.from_pretrained(FINE_TUNED_MODEL_NAME)
-    model.to(device)
-    model.eval()
-    model.zero_grad()
-    
-    tokenizer = BertTokenizer.from_pretrained(FINE_TUNED_MODEL_NAME)
-    
-    ref_token_id = tokenizer.pad_token_id # A token used for generating token reference
-    sep_token_id = tokenizer.sep_token_id # A token used as a separator between question and text and it is also added to the end of the text.
-    cls_token_id = tokenizer.cls_token_id # A token used for prepending to the concatenated question-text word sequence
-    
-    input_ids, ref_input_ids, sep_id = construct_input_ref_pair(text, ref_token_id, sep_token_id, cls_token_id)
-    token_type_ids, ref_token_type_ids = construct_input_ref_token_type_pair(input_ids, sep_id)
-    attention_mask = construct_attention_mask(input_ids)
-
-    logits = predict(input_ids, token_type_ids=token_type_ids, attention_mask=attention_mask)
-    
-    lig = LayerIntegratedGradients(sentclass_pos_forward_func, model.bert.embeddings)
-    
-    attributions_start, delta_start = lig.attribute(inputs=input_ids,
-                                      baselines=ref_input_ids,
-                                      additional_forward_args=(token_type_ids, attention_mask),
-                                      return_convergence_delta=True)
-    
-    attributions_start_sum = summarize_attributions(attributions_start)
-    
-    indices = input_ids[0].detach().tolist() # take off from cuda to cpu
-    all_tokens = tokenizer.convert_ids_to_tokens(indices)
-    print('Review sentence: ', text)
-    print(f'Predicted Answer: {torch.argmax(logits[0])} vs. Gold: {ground_truth}')
-    for t, a in zip(all_tokens, attributions_start_sum.detach().tolist()):
-        print(t + '\t\t' + str(a))
-            
-    
-    
+     
 
 """
 # storing couple samples in an array for visualization purposes
